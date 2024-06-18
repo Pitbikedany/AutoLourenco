288124ea2cadfce4c17a719d1dffef4c1855a495
Daniel Santos Lourenço <danysloureco@gmail.com>
2024-06-18 09:03:13 +0100

    Update

--- AutoLourenco/app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Cliente,Agenda,Servicos,Mecanico
-
-admin.site.register(Cliente)
-admin.site.register(Agenda)
-admin.site.register(Servicos)
-admin.site.register(Mecanico)
